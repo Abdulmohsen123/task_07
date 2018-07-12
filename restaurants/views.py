dc7dd814366acb84ecd68778ddfc2b3a7b0c7a34
Abdulmohsen <training.coded@gmail.com>
2018-07-12 15:34:11 +0300

    removed print statement

--- restaurants/views.py
+++ restaurants/views.py
@@ -39,7 +39,6 @@ def restaurant_update(request, restaurant_id):
     restaurant = Restaurant.objects.get(id=restaurant_id)
     form = RestaurantForm(instance=restaurant)
     if request.method == 'POST':
-        print("Inside IF Statement")
         form = RestaurantForm(request.POST, instance=restaurant)
         if form.is_valid():
             form.save()
